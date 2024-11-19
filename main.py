e89775bbb8d63243b860f894e5ee15d482b2dc15
Dis <dis-3-3@protonmail.com>
2024-11-19 22:30:00 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,79 @@
+import uuid
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse, FileResponse
+ 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.id = str(uuid.uuid4())
+ 
+people = [
+    Person("Tom", 38),
+    Person("Bob", 42),
+    Person("Sam", 28)
+]
+ 
+# для поиска пользователя в списке people
+def find_person(id):
+   for person in people: 
+        if person.id == id:
+           return person
+ 
+app = FastAPI()
+ 
+@app.get("/")
+async def main():
+    return FileResponse("public/index.html")
+ 
+@app.get("/api/users")
+def get_people():
+    return people
+ 
+@app.get("/api/users/{id}")
+def get_person(id):
+    person = find_person(id)
+    print(person)
+    
+    if person == None:  
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            content={ "message": "Пользователь не найден" }
+        )
+    
+    return person
+ 
+@app.post("/api/users")
+def create_person(data = Body()):
+    person = Person(data["name"], data["age"])
+    people.append(person)
+    return person
+ 
+@app.put("/api/users")
+def edit_person(data = Body()):
+    person = find_person(data["id"])
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person == None: 
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            content={ "message": "Пользователь не найден" }
+        )
+    
+    person.age = data["age"]
+    person.name = data["name"]
+    return person
+ 
+@app.delete("/api/users/{id}")
+def delete_person(id):
+    # получаем пользователя по id
+    person = find_person(id)
+  
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person == None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            content={ "message": "Пользователь не найден" }
+        )
+  
+    people.remove(person)
+    return person
